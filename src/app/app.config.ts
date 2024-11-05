a6c9c7efec287f88d68d0f3943494be66e562002
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-11-05 11:15:09 +0000

    Configured DevTools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,19 @@ import { provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
 
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
 import { todoReducer } from './todos/todo.reducer';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideStore({ todos: todoReducer })
+    provideStore({ todos: todoReducer }),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: environment.production // Enable log-only mode in production
+    })
   ]
 };
