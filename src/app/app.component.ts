14480803a6175337ce2b96ba8257a7a04cf4a807
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-11-05 09:16:10 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { TodoModule } from './todos/todo.module';
+import { TodoListComponent } from './todos/todo-list/todo-list.component';
+import { TodoListItemComponent } from './todos/todo-list-item/todo-list-item.component';
+import { TodoAddComponent } from './todos/todo-add/todo-add.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TodoModule,
+    TodoListComponent,
+    TodoListItemComponent,
+    TodoAddComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
