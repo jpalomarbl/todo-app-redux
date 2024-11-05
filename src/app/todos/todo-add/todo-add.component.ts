55bc2375e07f92f17db10cfbe15dbfc8ae9c5acd
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-11-05 12:04:31 +0000

    Finished todo-add component

--- src/app/todos/todo-add/todo-add.component.ts
+++ src/app/todos/todo-add/todo-add.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+
+import * as actions from '../todo.actions';
+import { AppState } from '../../app.reducer';
 
 @Component({
   selector: 'app-todo-add',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './todo-add.component.html',
   styleUrl: './todo-add.component.scss'
 })
 export class TodoAddComponent {
+  titleInput: FormControl;
 
+  constructor(private store: Store<AppState>) {
+    this.titleInput = new FormControl('', Validators.required);
+  }
+
+  addTodoTask(): void {
+    if (this.titleInput.valid) {
+      this.store.dispatch(actions.createTodo({ title: this.titleInput.value }));
+      this.titleInput.reset();
+    }
+  }
 }
