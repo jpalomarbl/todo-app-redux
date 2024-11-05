e09a84ab5c81898ff7be68421e97160a82c37cb6
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-11-05 09:49:07 +0000

    Configured store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
 
+import { todoReducer } from './todos/todo.reducer';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore({ todos: todoReducer })
+  ]
 };
